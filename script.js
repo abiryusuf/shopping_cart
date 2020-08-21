62f20cc4049528f099d4020f78d96d8d8fcc4fea
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-20 20:29:46 -0500

    JS

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const plusBtn = document.getElementById("plusBtn");
 plusBtn.addEventListener("click", function(){
     //console.log("click btn")
+    const increase = document.getElementById("increase");
+    const increaseCount = parseInt(increase.value);
+    const newCount = increaseCount + 1;
+    increase.value = newCount;
+    const phoneTotal = newCount * 1219;
+    const total = document.getElementById("phone-total").innerText = phoneTotal;
+    console.log(phoneTotal);
 })
