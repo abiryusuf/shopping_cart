7065b86ad790d3a6565e0f99128d3ba0f1d069aa
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-20 21:16:13 -0500

    increase Button

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const plusBtn = document.getElementById("plusBtn");
 plusBtn.addEventListener("click", function(){
     //console.log("click btn")
-    const increase = document.getElementById("increase");
+    const increase = document.getElementById("phone-count");
     const increaseCount = parseInt(increase.value);
     const newCount = increaseCount + 1;
     increase.value = newCount;
     const phoneTotal = newCount * 1219;
-    const total = document.getElementById("phone-total").innerText = phoneTotal;
-    console.log(phoneTotal);
+    const total = document.getElementById("phone-total").innerText = "$" + phoneTotal;
+    //console.log(total);
+});
+
+document.getElementById("minusBtn").addEventListener("click", function(){
+    const decrease = document.getElementById("phone-count");
+    const phoneCount = parseInt(decrease.value);
+    const newCount = phoneCount - 1;
+    decrease.value = newCount;
+    const total = newCount * 1219;
+    document.getElementById("phone-total").innerText = "$" + total;
 })
