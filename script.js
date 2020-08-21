675599c05cf9a6781373577e68d6a0eed7dbd1b7
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-21 11:28:48 -0500

    case

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ document.getElementById("minusBtn").addEventListener("click", function(){
     const total = newCount * 1219;
     document.getElementById("phone-total").innerText = "$" + total;
 })
+
+//case
+const caseBtn = document.getElementById("caseBtn");
+caseBtn.addEventListener("click", function(){
+    const caseInput = document.getElementById("case-input");
+    const caseCount = parseInt (caseInput.value);
+    const newCase = caseCount + 1;
+    caseInput.value = newCase;
+    const total = newCase * 59;
+    document.getElementById("price").innerText = "$" + total;
+    //console.log(caseCount);
+});
+
